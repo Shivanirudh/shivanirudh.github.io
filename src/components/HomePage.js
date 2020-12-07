d2bcf2344077bdf6868b00a3dff37b1d42dccb7b
Shivanirudh <shivanirudh2001@gmail.com>
2020-12-07 12:33:01 +0530

    Home page tweaks

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import {config, Spring} from "react-spring/renderprops";
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {faInstagram} from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 import ProPic from "./images/Profile.jpg";
 
@@ -20,11 +25,19 @@ export class HomePage extends Component {
                     <p style={{fontSize: "40px"}}> 
                         Nice to meet you! <br/> 
                         Feel free to look around my portfolio and reach out.<br/> 
-                        <b>Thanks for stopping by!</b>
+                        <b>Thanks for stopping by!</b><br/>
                     </p>
                 </h2>
             </span>
-           
+            <div className="social-media-icons">
+                <a href="mailto:shivanirudh2001@gmail.com" target="blank"><FontAwesomeIcon icon={faEnvelope}/></a>
+                &nbsp;
+                <a href="https://www.linkedin.com/in/shivanirudh-s-g/" target="blank"><FontAwesomeIcon icon={faLinkedin}/></a>
+                &nbsp;
+                <a href="https://github.com/Shivanirudh" target="blank"><FontAwesomeIcon icon={faGithub}/></a>
+                &nbsp;
+                <a href="https://instagram.com/shiv_anirudh" target="blank"><FontAwesomeIcon icon={faInstagram}/></a>
+            </div>
             
             </div>
         )
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,21 +7,16 @@ import {faHome} from '@fortawesome/free-solid-svg-icons';
 import {faInfoCircle} from '@fortawesome/free-solid-svg-icons';
 import {faHistory} from '@fortawesome/free-solid-svg-icons';
 
-import {faGithub} from '@fortawesome/free-brands-svg-icons';
-import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
-import {faInstagram} from '@fortawesome/free-brands-svg-icons';
-import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
-
 export class Navigation extends Component {
     render() {
         return (
             <div className="sidebar">
                 <ul className="sidebar-links">
                     <li className="menu-icon">
-                        <Link className="sidebar-item" to="#">
+                        <div className="sidebar-item" to="#">
                             <span className="sidebar-item-name menu-icon-name">Map</span>
                             <FontAwesomeIcon className="logo-icon" icon={faAngleDoubleRight} style={{fontSize:"40px"}} rotation={180}/>
-                        </Link>
+                        </div>
                     </li>
                     <li className="link">
                         <Link className="sidebar-item" to="/">
@@ -41,18 +36,6 @@ export class Navigation extends Component {
                             <FontAwesomeIcon className="link-icon" icon={faHistory} style={{fontSize:"40px"}}/>
                             <span className="sidebar-item-name">Timeline</span>
                         </Link>
-                    </li>
-                    {/*Social Media*/}
-                    <li className="link">
-                        <div className="social-media-icons" to="#">
-                            <a href="mailto:shivanirudh2001@gmail.com" target="blank"><FontAwesomeIcon icon={faEnvelope}/></a>
-                            &nbsp;
-                            <a href="https://www.linkedin.com/in/shivanirudh-s-g/" target="blank"><FontAwesomeIcon icon={faLinkedin}/></a>
-                            &nbsp;
-                            <a href="https://github.com/Shivanirudh" target="blank"><FontAwesomeIcon icon={faGithub}/></a>
-                            &nbsp;
-                            <a href="https://instagram.com/shiv_anirudh" target="blank"><FontAwesomeIcon icon={faInstagram}/></a>
-                        </div>
                     </li>
                 </ul>
             </div>
