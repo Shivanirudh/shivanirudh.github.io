2227574ddaea6b5200e3b44862bd64d54254ce64
Shivanirudh <shivanirudh2001@gmail.com>
2020-12-09 23:30:36 +0530

    Updated CV and owner info

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,36 +15,40 @@ export class HomePage extends Component {
 
         return (
             <div>
-            <Zoom cascade>    
-            <div className="header">
-                <h1>Welcome</h1>
-            </div>
-            </Zoom>
-            <span className="intro">
-                <Zoom cascade>
-                    <div>
-                        <img  className="intro-picture" src={ProPic} alt="ProfilePicture" height="350px" width="350px" style={{borderRadius:"50%"}}/>
-                    </div>
-                    <div>
-                    <h2>I am <b>SHIVANIRUDH</b>.<br/>
-                        <p style={{fontSize: "40px"}}> 
-                            Nice to meet you! <br/> 
-                            Feel free to look around my portfolio and reach out.<br/> 
-                            <b>Thanks for stopping by!</b><br/>
-                        </p>
-                    </h2>
-                    </div>
+                <Zoom cascade>    
+                <div className="header">
+                    <h1>Welcome</h1>
+                </div>
                 </Zoom>
-            </span>
-            <div className="social-media-icons">
-                <a href="mailto:shivanirudh2001@gmail.com" target="blank"><FontAwesomeIcon icon={faEnvelope}/></a>
-                &nbsp;
-                <a href="https://www.linkedin.com/in/shivanirudh-s-g/" target="blank"><FontAwesomeIcon icon={faLinkedin}/></a>
-                &nbsp;
-                <a href="https://github.com/Shivanirudh" target="blank"><FontAwesomeIcon icon={faGithub}/></a>
-                &nbsp;
-                <a href="https://instagram.com/shiv_anirudh" target="blank"><FontAwesomeIcon icon={faInstagram}/></a>
-            </div>
+                <span className="intro">
+                    
+                    <div>
+                        <Zoom cascade>
+                            <img  className="intro-picture" src={ProPic} alt="ProfilePicture" height="350px" width="350px" style={{borderRadius:"50%"}}/>
+                        </Zoom>
+                    </div>
+                    <div>
+                        <Zoom cascade> 
+                            <h2>
+                                I am <b>SHIVANIRUDH</b>.
+                            </h2>
+                            <h2 style={{fontSize: "40px"}}> 
+                                Nice to meet you! <br/> 
+                                Feel free to look around my portfolio and reach out.<br/> 
+                                <b>Thanks for stopping by!</b><br/>
+                            </h2>
+                        </Zoom>
+                    </div>
+                </span>
+                <div className="social-media-icons">
+                    <a href="mailto:shivanirudh2001@gmail.com" target="blank"><FontAwesomeIcon icon={faEnvelope}/></a>
+                    &nbsp;
+                    <a href="https://www.linkedin.com/in/shivanirudh-s-g/" target="blank"><FontAwesomeIcon icon={faLinkedin}/></a>
+                    &nbsp;
+                    <a href="https://github.com/Shivanirudh" target="blank"><FontAwesomeIcon icon={faGithub}/></a>
+                    &nbsp;
+                    <a href="https://instagram.com/shiv_anirudh" target="blank"><FontAwesomeIcon icon={faInstagram}/></a>
+                </div>
             
             </div>
         )
--- src/components/Owner.js
+++ src/components/Owner.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import Zoom from 'react-reveal';
+
 
 import ProPic from "./images/Profile.jpg";
+import CV from "./images/Shivanirudh.pdf"
 
 export class Owner extends Component {
     render() {
         return (
             <div className="owner">
-                <h1 className="owner-head">Owner Info</h1>
+                <Zoom cascade>
+                    <h1 className="owner-head">Owner Info</h1>
+                </Zoom>
+                <Zoom cascade>
                 <span className="name-pic">
                     <div className="picture">
                         <img src={ProPic} alt="ProfilePicture" height="250px" width="250px" style={{borderRadius:"50%"}}/>
@@ -14,24 +20,32 @@ export class Owner extends Component {
                     <div className="details">
                         <p align="center" className="item">
                             <b className="item-head">Name  :</b> &nbsp;&nbsp;
-                            <dummy className="item-value">Shivanirudh S G</dummy>
+                            <span className="item-value">Shivanirudh S G</span>
                         </p>
                         <p align="center" className="item">
                             <b className="item-head">Role  :</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <dummy className="item-value">Programmer/Software Developer</dummy>
+                            <span className="item-value">Programmer/Software Developer</span>
                         </p>
                     </div>
                 </span>
+                </Zoom>
                 <div className="bio">
-                    <h2 className="bio-head">Bio:</h2>
-                    <div className="bio-content">
-                        R, python, javascript, shiny, dplyr, purrr, ditto, ggplot, d3, canvas, spark, sawk, pyspark,
-                        sparklyR, lodash, lazy, bootstrap, jupyter, vulpix, git, flask, numpy, pandas, feebas, 
-                        scikit, bgm, bayes, h20.ai, sparkling-water, tensorflow, keras, onyx, ekans, hadoop, scala, 
-                        unity, gc, metapod, c#/c++, krebase, neo4j. <br/>
-                        
-                        I typically ask recruiters to point out which of these are pokemon.<img className="pokeball" src="https://img.icons8.com/color/25/000000/pokeball.png" alt="pokeball" />
-                    </div>
+                    <Zoom cascade>
+                        <div className="bio-content">
+                            R, python, javascript, shiny, dplyr, purrr, <span className="answer">ditto</span>, ggplot, d3, canvas, spark, <span className="answer">sawk</span>, pyspark,
+                            sparklyR, lodash, lazy, bootstrap, jupyter, <span className="answer">vulpix</span>, git, flask, numpy, pandas, <span className="answer">feebas</span>, 
+                            scikit, bgm, bayes, h20.ai, sparkling-water, tensorflow, keras, <span className="answer">onyx</span>, <span className="answer">ekans</span>, hadoop, scala, 
+                            unity, gc, <span className="answer">metapod</span>, c#/c++, krebase, neo4j. <br/>
+                            
+                            I typically ask recruiters to point out which of these are pokemon.<img className="pokeball" src="https://img.icons8.com/color/25/000000/pokeball.png" alt="pokeball" />
+                        </div>
+                        <h2 className="bio-head">Bio:</h2>
+                        <div className="bio-interests">
+                            I am currently a junior at <a href="https://www.ssn.edu.in"><u>SSN College of Engineering</u></a> pursuing my undergraduate education in <b>Computer Science and Engineering</b>. 
+                            I am interested in the fields of <b>Machine Learning</b>, <b>Data Science</b> and <b>Natural Language Processing</b>.
+                            Click <a href={CV} target="blank"><u>here</u></a> to view my resume.
+                        </div>
+                    </Zoom>
                 </div>
                 
             </div>
